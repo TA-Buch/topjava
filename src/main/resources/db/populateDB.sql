abb244bf2d2c97e4bd4b531ce53c6a9db3571bd8
Tatiana_Zarifova <buch_ta@mail.ru>
2025-02-26 20:28:46 +0500

    hw3- JDBC + JUNIT

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,7 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM user_role;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +12,7 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2024-02-21 12:30:00', 'Lunch - Chicken and Rice', 600),
+       (100001, '2024-02-21 08:00:00', 'Breakfast - Oatmeal and Fruits', 400);
