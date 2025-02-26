e15801b2a36d7f6555877c7afee629f90374701a
Tatiana_Zarifova <buch_ta@mail.ru>
2025-02-26 20:28:46 +0500

    hw3- JDBC + JUNIT

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -16,7 +16,7 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int userId = Integer.parseInt(request.getParameter("userId"));
+        int userId = 100000 + Integer.parseInt(request.getParameter("userId"));
         SecurityUtil.setAuthUserId(userId);
         response.sendRedirect("meals");
     }
