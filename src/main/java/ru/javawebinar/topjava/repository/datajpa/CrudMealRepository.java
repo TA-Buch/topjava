0201eab65b69a3da00469e1222fb47bd89dfc91a
Tatiana_Zarifova <buch_ta@mail.ru>
2025-03-07 18:27:38 +0500

    hw5- spring data JPA

---
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.topjava.repository.datajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.List;
+
+public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+}
